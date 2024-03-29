9dd3b5045ed3dacd825754ab9c1355061974c317
windsspptman <165377626+windsspptman@users.noreply.github.com>
2024-03-29 17:27:11 +0530

    Add files via upload

---
+++ W01in0sNw0Sa2Er087/js/analytics.js
@@ -0,0 +1,9 @@
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXX-X';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-XXXXXXXXX-X');
